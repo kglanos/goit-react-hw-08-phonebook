194bd29d795c7532e49449e8af66d351a7d11676
kglanos <k.glanowski@wp.pl>
2024-01-14 02:30:50 +0100

    poprawka

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
             </Helmet>
             <Container maxW={'2xl'} centerContent>
                 <Center>
-                <Box padding={'100px'} mt={'10'}>
+                <Box padding={'100px'} mt={'10'} textAlign={'center'}>
                     <Image src={'https://img.freepik.com/free-vector/contact-us-concept-landing-page_52683-11237.jpg?w=996&t=st=1705185625~exp=1705186225~hmac=e15d3def7a582acb35f3a00ce8f8c9b52485d408f7b1afb2239bdd3b1ec0c943'} alt={'Contact Us Image'} mb={'4'} />
                     <Heading as={'h2'} fontSize={'xl'}>
                     Contacts Book welcome page
