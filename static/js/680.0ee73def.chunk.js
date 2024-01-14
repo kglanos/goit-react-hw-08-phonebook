977dcadd5f24cf60c0fda3bc09f026de91213e04
kglanos <130902847+kglanos@users.noreply.github.com>
2024-01-14 01:27:59 +0000

    Deploying to gh-pages from @ kglanos/goit-react-hw-08-phonebook@fa070188cb3a287f9b447243b07398106c0a3b91 🚀

--- static/js/680.0ee73def.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[680],{680:function(e,n,o){o.r(n);var a=o(184);n.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("p",{children:"Task manager welcome page"})})}}}]);
-//# sourceMappingURL=680.0ee73def.chunk.js.map
